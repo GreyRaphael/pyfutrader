34c76dd6044d28cca3686bc6c49ee168684853da
BeC9_gewei <grey@pku.edu.cn>
2025-05-19 02:32:45 +0800

    add SMA

--- quote_receiver.py
+++ quote_receiver.py
@@ -1,17 +1,22 @@
 from config import utils
 import pynng
 from datatype import quote
+from ta import rolling
 
 
 def main():
+    smaer = rolling.Meaner(5)
+
     nng_config = utils.read_config("config/nng.toml")
     # empty topics to receive all messages
     with pynng.Sub0(dial=nng_config["Address"], topics=b"") as sub:
         while True:
             binary_msg = sub.recv()
             tick = quote.TickData.from_buffer_copy(binary_msg)
-            print(tick)
-            print(tick.to_dict())
+            print(tick.InstrumentID, f"{tick.TradingDay + b' ' + tick.UpdateTime}.{tick.UpdateMillisec}", tick.LastPrice)
+            # print(tick.to_dict())
+            ma5 = smaer.update(tick.LastPrice)
+            print(f"======>ma5={ma5}")
 
 
 if __name__ == "__main__":
